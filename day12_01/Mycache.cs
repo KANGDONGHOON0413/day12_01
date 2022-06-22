21f627c659a25055a9f76ec6c92ca7ddae145c80
KANGDONGHOON0413 <kmerits@gmail.com>
2022-06-22 16:29:18 +0900

    초기 커밋.

---
+++ day12_01/Mycache.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day12_01
+{
+    class Mycache
+    {
+        //Dictionary: key -> Value
+        private Dictionary<string, string> cache; // key타입: str, val 타입: str
+        public Mycache()
+        {
+            cache = new Dictionary<string, string>();
+            //default cache value;
+            cache.Add("Debug", "false");
+            cache.Add("Logging", "true");
+        }
+
+        public void Add(string key, string val) //add
+        {
+            if (!cache.ContainsKey(key)) cache[key] = val;
+            else throw new ApplicationException("key already exists");
+        }
+
+
+
+        //속성=매개변수를 가질 수 없다
+        //예시
+        private DateTime cacheExpires;
+        public DateTime CacheExpires
+        {
+            get { return cacheExpires; }
+            set { cacheExpires = value; }
+        }
+        //end 예시
+
+
+        //인덱서를 이용한 경우 밑에와 같은 결과를 갖는다.
+        public string this[string key]
+        {
+            get
+            {
+                if (cache.ContainsKey(key)) return cache[key];
+                return null;
+            }
+            set
+            {
+                if (cache.ContainsKey(key)) cache[key] = value;
+                else throw new ApplicationException("key not found");
+            }
+        }
+        
+
+        public string getItem(string key)   //get
+        {
+            if (cache.ContainsKey(key)) return cache[key];
+            return null;
+        }
+
+        public void setItem(string key, string val) //update
+        {
+            if (cache.ContainsKey(key)) cache[key]= val;
+            else throw new ApplicationException("key not found");
+        }
+    }
+}
---
+++ day12_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day12_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myche = new Mycache();
+            myche.Add("ItemId","1100");
+            var t1 = myche.getItem("Debug");
+            myche.setItem("ItemId", "1200");
+            var t2 = myche.getItem("ItemId");
+            
+            //인덱서 활용 부분
+            myche["Debug"] = "true";
+            var t3 = myche["Debug"];
+            
+            Console.WriteLine(t1 + "\t" + t2 + "\t" + t3);
+        }
+    }
+}
